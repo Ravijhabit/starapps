e1bb4a2f70f97de4e215bba1fca2b78b29a3a989
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2023-04-17 23:37:24 +0530

    added functionality

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ const colorValues = [
         image:'./assets/Yello umbrella.png'
     }
 ];
-// let activeColor = '#03d3fc';
 const image = document.querySelector('.imageContainer>img');
+const imageContainer = document.querySelector('.imageContainer');
+const loader = document.querySelector('.loader');
 body.style.background='#03d3fc29';
 inputHandler.style.background='#03d3fc';
 const colorPalette = document.querySelector('.colorPalette');
+let prevTimer,newTimer;
 for(let colors of colorValues){
     let el = document.createElement('div');
     el.style.background=colors.color;
@@ -26,20 +28,52 @@ for(let colors of colorValues){
     el.style.width='20px';
     el.style.height='20px';
     el.style.cursor='pointer';
-    el.style.border=`5px solid ${colors.color}`;
     el.style.backgroundClip='padding-box';
+    el.style.border=`5px solid ${colors.color}`;
     el.onclick = function(){
+        clearTimeout(prevTimer);
+        if(imageContainer.children.length>2)
+            imageContainer.removeChild(imageContainer.children[2]);
         const traverse = document.querySelector('.colorPalette');
-        console.dir(traverse);
         for(const trav of traverse.children){
-            console.log(trav);
-            trav.style.border=`5px solid ${trav.style.background}`;
+            trav.style.border=`5px solid ${trav.style.backgroundColor}`;
         }
+        image.style.display='none';
+        loader.style.display='flex';
+        loader.children[0].style.fill=colors.color;
+        prevTimer = setTimeout(()=>{
+            image.style.display='block';
+            loader.style.display='none';
+        },3000);
+        image.src=colors.image;
         inputHandler.style.background = colors.color;
         body.style.background = colors.color+'29';
-        image.src=colors.image;
         el.style.border=`5px solid ${colors.color+'59'}`;
-        el.style.boxSizing='borderbox';
     }
     colorPalette.appendChild(el);
+}
+let form = document.querySelector('#logoContainer');
+form.addEventListener('change', handleFiles, false);
+function handleFiles(){
+    const fileList = this.files;
+    clearTimeout(newTimer);
+    const image = document.querySelector('.imageContainer>img');
+    const loader = document.querySelector('.loader');
+    const companyName = document.createElement('img');
+    const imageContainer = document.querySelector('.imageContainer');
+    if(imageContainer.children.length>2)
+        imageContainer.removeChild(imageContainer.children[2]);
+    companyName.src = URL.createObjectURL(fileList[0]);
+    companyName.onload = () =>{
+        URL.revokeObjectURL(companyName.src);
+    };
+    companyName.classList.add('companyName');
+    image.style.display='none';
+    loader.children[0].style.fill=document.querySelector('#logo').style.background;
+    loader.style.display='flex';
+    newTimer = setTimeout(()=>{
+        image.style.display='block';
+        loader.style.display='none';
+        imageContainer.appendChild(companyName);
+    },3000);
 }
